c33986e358fb587c12e0638cbc3b9af505392fd3
Umberto Arduini <umberto.arduini@abstract.it>
2022-01-29 03:07:54 +0100

    Added variable length and add segment method

--- Assets/Script/SnakeController.cs
+++ Assets/Script/SnakeController.cs
@@ -2,67 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeController : MonoBehaviour {
-
+public class SnakeController : MonoBehaviour
+{
     // Settings
     public float MoveSpeed = 5;
+
     public float SteerSpeed = 180;
+
     public float BodySpeed = 5;
+
     public int Gap = 10;
+
+    public int InitialLength = 3;
+
+    public int Length;
 
     // References
     public GameObject SnakePrefab;
+
     public GameObject BodyPrefab;
 
     // Lists
     private List<GameObject> BodyParts = new List<GameObject>();
+
     private List<Vector3> PositionsHistory = new List<Vector3>();
 
     // Start is called before the first frame update
-    void Start() {
-        GrowSnake();
-        GrowSnake();
-        GrowSnake();
-        GrowSnake();
-        GrowSnake();
-        GrowSnake();
+    void Start()
+    {
+        Length = InitialLength;
+        for (int i = 0; i < InitialLength; i++)
+        {
+            GrowSnake();
+        }
+    }
+
+    public void AddNewSegment()
+    {
+        Length++;
         GrowSnake();
     }
 
     // Update is called once per frame
-    void Update() {
-
+    void Update()
+    {
         // Move forward
-        SnakePrefab.transform.position += SnakePrefab.transform.forward * MoveSpeed * Time.deltaTime;
+        SnakePrefab.transform.position +=
+            SnakePrefab.transform.forward * MoveSpeed * Time.deltaTime;
 
         // Steer
         float steerDirection = Input.GetAxis("Horizontal"); // Returns value -1, 0, or 1
-        SnakePrefab.transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
+        SnakePrefab
+            .transform
+            .Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
 
         // Store position history
         PositionsHistory.Insert(0, SnakePrefab.transform.position);
 
         // Move body parts
         int index = 0;
-        foreach (var body in BodyParts) {
-            Vector3 point = PositionsHistory[Mathf.Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
+        foreach (var body in BodyParts)
+        {
+            Vector3 point =
+                PositionsHistory[Mathf
+                    .Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
 
             // Move body towards the point along the snakes path
             Vector3 moveDirection = point - body.transform.position;
-            body.transform.position += moveDirection * BodySpeed * Time.deltaTime;
+            body.transform.position +=
+                moveDirection * BodySpeed * Time.deltaTime;
 
             // Rotate body towards the point along the snakes path
-            body.transform.LookAt(point);
+            body.transform.LookAt (point);
 
             index++;
         }
     }
 
-    private void GrowSnake() {
+    private void GrowSnake()
+    {
         // Instantiate body instance and
         // add it to the list
         GameObject body = Instantiate(BodyPrefab);
         body.transform.SetParent(this.gameObject.transform);
-        BodyParts.Add(body);
+        BodyParts.Add (body);
     }
 }
